e66e738ceffa0bf0e53ddd64617a11dca62d6d96
Sajidul Kabir <sajidulkabiradib@gmail.com>
2022-03-27 00:23:16 +0600

    SOLID integrated in printing vehicle details

---
+++ src/main/java/com/mycompany/vehicledetails/Interfaces/VehicleWithDoors.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.vehicledetails.Interfaces;
+
+import com.mycompany.vehicledetails.Interfaces.VehicleInterface;
+
+/**
+ *
+ * @author OMEN
+ */
+public interface VehicleWithDoors extends VehicleInterface{
+    public int numberOfDoors();
+}
